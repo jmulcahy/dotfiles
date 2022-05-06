97a1dc9fef261c8e20a80b41ccd417ecc74abfad
John Mulcahy <6724499+jmulcahy@users.noreply.github.com>
2022-05-06 00:18:29 -0600

    Set JULIA_NUM_THREADS

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,7 +1,5 @@
-atreplinit() do repl
-    try
-        @eval using Revise
-        @async Revise.wait_steal_repl_backend()
-    catch
-    end
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise" exception=(e, catch_backtrace())
 end
--- .zshenv
+++ .zshenv
@@ -7,3 +7,5 @@ export PATH="$HOME/.cargo/bin:$HOME/rtags/bin/:$HOME/.local/bin:/Users/johnmulca
 export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 
 export PATH_SAVE=$PATH
+
+export JULIA_NUM_THREADS=8
